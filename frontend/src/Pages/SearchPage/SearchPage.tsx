fff28a792610f6fd7b8a653a140cae035b2b4592
Matthew Yim <Matthew.Yim@ISGInc.com>
2024-07-02 16:48:26 -0500

    Working Commit for Page/Route Implementation

--- frontend/src/Pages/SearchPage/SearchPage.tsx
+++ frontend/src/Pages/SearchPage/SearchPage.tsx
@@ -1,9 +1,69 @@
-import React from "react";
+import React, { useState, ChangeEvent, SyntheticEvent } from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import { CompanySearch } from "../../company";
+import { searchCompanies } from "../../api";
+import Search from "../../Components/Search/Search";
+import ListPortfolio from "../../Components/Portfolio/ListPortfolio/ListPortfolio";
+import CardList from "../../Components/CardList/CardList";
 
 interface Props {}
 
 const SearchPage = (props: Props) => {
-  return <div>SearchPage</div>;
+  const [search, setSearch] = useState<string>(""); // you can have ts either infer it(no generics) or use generics to set type checking
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]); // API call we made it so it returns either string array or a string
+  const [serverError, setServerError] = useState<string | null>(null); // Remember the API call that errors returns a string
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value); // going into input and getting value
+    console.log(e);
+  };
+
+  const onPortfolioCreate = (e: any) => {
+    // "e: any" turns of type script or type checking portion
+    e.preventDefault();
+    const exists = portfolioValues.find((value) => value === e.target[0].value);
+    if (exists) return;
+    const updatedPortfolio = [...portfolioValues, e.target[0].value];
+    setPortfolioValues(updatedPortfolio); // Reason for this, is cuz React doesn't like mutable arrays, wants a new array to re-render
+  };
+
+  const onPortfolioDelete = (e: any) => {
+    e.preventDefault();
+    const removed = portfolioValues.filter((value) => {
+      return value !== e.target[0].value;
+    });
+    setPortfolioValues(removed);
+  };
+  const onSearchSubmit = async (e: SyntheticEvent) => {
+    // If you can't get event MouseEvent<HTMLButtonElement, MouseEvent> to work use "e: SyntheticEvent"
+    e.preventDefault();
+    const result = await searchCompanies(search);
+    if (typeof result === "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    }
+    console.log(searchResult);
+  };
+  return (
+    <>
+      <Search
+        onSearchSubmit={onSearchSubmit}
+        search={search}
+        handleSearchChange={handleSearchChange}
+      />
+      <ListPortfolio
+        portfolioValues={portfolioValues}
+        onPortfolioDelete={onPortfolioDelete}
+      />
+      <CardList
+        searchResults={searchResult}
+        onPortfolioCreate={onPortfolioCreate}
+      />
+      {serverError && <div>Unable to connect to API</div>}
+    </>
+  );
 };
 
 export default SearchPage;
